cb3585e12151bd29710148e1b447cfc1e1ac9419
Ydpatel12 <155326501+Ydpatel12@users.noreply.github.com>
2024-09-29 22:50:51 -0400

    BinaryHeap file
    Extra Credit for implementing our own binary heap

---
+++ binaryHeap.py
@@ -0,0 +1,53 @@
+class BinaryHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, item):
+        """Add item to the heap, maintaining the heap invariant."""
+        self.heap.append(item)
+        self._sift_up(len(self.heap) - 1)
+
+    def pop(self):
+        """Remove and return the smallest item from the heap."""
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()  # Move the last item to the root
+        self._sift_down(0)
+        return root
+
+    def _sift_up(self, index):
+        """Move the item at index up to its proper position in the heap."""
+        item = self.heap[index]
+        while index > 0:
+            parent_index = (index - 1) >> 1
+            parent = self.heap[parent_index]
+            if item < parent:
+                self.heap[index] = parent
+                index = parent_index
+                continue
+            break
+        self.heap[index] = item
+
+    def _sift_down(self, index):
+        """Move the item at index down to its proper position in the heap."""
+        item = self.heap[index]
+        end_index = len(self.heap)
+        child_index = 2 * index + 1  # Left child index
+        while child_index < end_index:
+            right_child_index = child_index + 1
+            if right_child_index < end_index and not self.heap[child_index] < self.heap[right_child_index]:
+                child_index = right_child_index
+            if self.heap[child_index] < item:
+                self.heap[index] = self.heap[child_index]
+                index = child_index
+                child_index = 2 * index + 1
+            else:
+                break
+        self.heap[index] = item
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __repr__(self):
+        return f'{self.heap}'
